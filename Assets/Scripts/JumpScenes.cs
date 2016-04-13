87e31a71ce56681de499ff87bb77457c4e2cdb07
nathangl <nathangl@sbcglobal.net>
2016-04-13 18:38:59 -0400

    UI Update 4/13/16 Test Upload
    Includes left-side menu and decision menu.

---
+++ Assets/Scripts/JumpScenes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class JumpScenes : MonoBehaviour {
+
+        public void LoadScene(int level)
+        {
+            SceneManager.LoadScene(level);
+        }
+}
