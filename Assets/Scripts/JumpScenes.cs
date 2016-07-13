913e63d01696d76159d46dd43808839aa65253e3
nathangl <nathangl@sbcglobal.net>
2016-07-13 14:26:06 -0400

    Main Menu / Folders
    Main menu updated and folders organized.

--- Assets/Scripts/JumpScenes.cs
+++ Assets/Scripts/JumpScenes.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
+//Scenes change by int corresponding to Build Setting Index numbers
+
 public class JumpScenes : MonoBehaviour {
 
         public void LoadScene(int level)
