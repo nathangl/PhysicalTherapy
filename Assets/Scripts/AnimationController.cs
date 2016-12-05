f8498670de0ea3cc2e77fe8557fc949b5a3e39b8
nathangl <nathangl@sbcglobal.net>
2016-12-04 22:53:26 -0500

    Dropdown Implemented
    Dropdown menu in the project but not working yet. Patient Tree moved to
    PatientController.

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+    Animator patientAnim;
+
+    void Awake()
+    {
+        patientAnim = GetComponent<Animator>();
+    }
+
+    public void PlayPatientAnim(string pAnim)
+    {
+        if (pAnim == "leftShoulder")
+        {
+            Debug.Log("Animation Accessed");
+            //patientAnim.SetTrigger("LeftShoulder");
+        }
+    }
+}
--- Assets/Scripts/PatientController.cs
+++ Assets/Scripts/PatientController.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System;
 
-public class PatientTree : MonoBehaviour
-{
-
-    static TreeNode root = new TreeNode { Value = "Patient" };
+public class PatientController : MonoBehaviour {
 
     public Dropdown userDropdown;
+    public SphereCollider leftShoulder, rightShoulder;
+    private RaycastHit hit;
+    TreeNode root = new TreeNode { Value = "Patient" };
 
     void Start()
     {
         root.Nodes.Add(new TreeNode { Value = "UpperExtremity" });
         root.Nodes[0].Nodes.Add(new TreeNode { Value = "Shoulder" });
         root.Nodes[0].Nodes[0].Nodes.Add(new TreeNode { Value = "LeftShoulder" });
+        root.Nodes[0].Nodes[0].Nodes[0].Nodes.Add(new TreeNode { Value = "AROM" });
+        root.Nodes[0].Nodes[0].Nodes[0].Nodes.Add(new TreeNode { Value = "PROM" });
         root.Nodes[0].Nodes[0].Nodes.Add(new TreeNode { Value = "RightShoulder" });
         root.Nodes[0].Nodes.Add(new TreeNode { Value = "Elbow" });
         root.Nodes[0].Nodes.Add(new TreeNode { Value = "Hand" });
@@ -25,22 +27,37 @@ public class PatientTree : MonoBehaviour
         root.Nodes.Add(new TreeNode { Value = "Side" });
         root.Nodes[2].Nodes.Add(new TreeNode { Value = "LEFT" });
         root.Nodes[2].Nodes.Add(new TreeNode { Value = "RIGHT" });
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // if left mouse clicked
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // make a raycast to clicked position
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) // if the raycast hit something
+            {
+                 SetDropdown(hit.collider.transform.tag);   // if (hit.collider.transform.tag == "Patient")
+            }
+
+        }
 
     }
 
-    static public void DetermineClick(string clicked)
+    public void SetDropdown(string clicked)       //static
     {
+        List<string> dropdownList = new List<string>();
         Action<TreeNode> traverse = null;
-
         traverse = (n) => {
             if (n.Value == clicked)
             {
                 Debug.Log(n.Value + " CLICKED!");
+                dropdownList.Add("Decisions");
                 foreach (TreeNode nv in n.Nodes)
                 {
-                    Debug.Log(nv.Value);
+                    //Debug.Log(nv.Value);
+                    dropdownList.Add(nv.Value);
                 }
-
             }
             else
             {
@@ -50,12 +67,12 @@ public class PatientTree : MonoBehaviour
         };
 
         traverse(root);
-    }
 
-    public void DropdownUI()
-    {
         userDropdown.gameObject.SetActive(true);
         userDropdown.transform.position = new Vector3(Input.mousePosition.x + 80, Input.mousePosition.y - 15, Input.mousePosition.z);
+        userDropdown.ClearOptions();
+        userDropdown.AddOptions(dropdownList);
+
     }
 }
 
