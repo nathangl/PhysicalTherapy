a8468e7f925289b01aa2fbf4a929f30e358e4ce6
Patrick Sorrell <Patrick Sorrell>
2016-12-12 00:22:20 -0500

    Added Chart Scene

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+	public delegate void ClickAction();
+	public static event ClickAction OnClicked;
+
+	public void foo(string name)
+	{
+		if (OnClicked != null)
+			OnClicked ();
+	}
+}
