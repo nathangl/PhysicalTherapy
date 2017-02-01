298b5b3276dae009a2449e283a24e4cec9d50c50
Patrick Sorrell <Patrick Sorrell>
2017-02-01 18:45:40 -0500

    Testing:

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class test : MonoBehaviour
-{
+{/*
     bool finished = false;
     public Animator anim;
     private float pos = 0f;
@@ -70,75 +70,133 @@ public class test : MonoBehaviour
             }
         }
         */
-        /*
-    // Use this for initialization
-    private float radius;
-    public Image dot;
-    private Vector3 center;
-    private float angle;
-    private Vector3 max;
-    private Vector3 min;
+ /*
+// Use this for initialization
+private float radius;
+public Image dot;
+private Vector3 center;
+private float angle;
+private Vector3 max;
+private Vector3 min;
+public Animator anim;
+private float prevAng = -70;
+float count = 0f;
+void Start()
+{
+ min = new Vector3(131, -78, 0);
+ max = new Vector3(61, 147, 0);
+ //center = gameObject.transform.position;
+ Debug.Log(center);
+}
+void Update()
+{
+ if (Input.GetMouseButton(0))
+ {
+     RaycastHit hit;
+     var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+     if (Physics.Raycast(ray, out hit, 100))
+     {
+         if (hit.transform.tag == "LeftHand")
+         {
+             center = gameObject.transform.position;
+             Vector3 dir = (Input.mousePosition - center).normalized;
+             radius = Vector3.Distance(center, dot.transform.position);
+             //angle = Vector2.Angle(center, dir);
+             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+             prevAng = angle;
+             if (angle > -70 && angle < 64)
+             {
+                 if (angle - prevAng >= 0)
+                 {
+                     Debug.Log("hey");
+                     anim.speed = 0;
+                     count += 0.003f;
+                     if(count < 0) count = 0;
+                     anim.Play("PROMLeftArm", 0, count);
+                 }
+                 else
+                 {
+                     Debug.Log("ho");
+                     anim.speed = 0;
+                     count -= 0.003f;
+                     if (count < 0) count = 0;
+                     anim.Play("PROMLeftArm", 0, count);
+                 }
+                 dot.transform.position = (Vector2)center + ((Vector2)dir * radius);
+                 Debug.Log(dir);
+                 Debug.Log(dot.transform.position);
+                 Debug.Log(angle);
+             }
+         }
+     }
+
+ }
+}
+void MathStuff()
+{
+
+}
+
+}
+*/
+    public Camera camera;
+    Vector3 player;
+    float minDistance = float.PositiveInfinity;
+    float minPercent  = 0;
+    Vector3 pos;
     public Animator anim;
-    private float prevAng = -70;
-    float count = 0f;
     void Start()
     {
-        min = new Vector3(131, -78, 0);
-        max = new Vector3(61, 147, 0);
-        //center = gameObject.transform.position;
-        Debug.Log(center);
+        //iTween.MoveUpdate(gameObject, iTween.Hash("path", iTweenPath.GetPath("New Path 1")));
+        //iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Path1"), "time", 2));
+        iTween.PutOnPath(gameObject, iTweenPath.GetPath("Path1"), 0f);
+        //Debug.Log(iTween.PointOnPath(iTweenPath.GetPath("Path1"), 43f));
+
     }
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 100))
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
             {
-                if (hit.transform.tag == "LeftHand")
-                {
-                    center = gameObject.transform.position;
-                    Vector3 dir = (Input.mousePosition - center).normalized;
-                    radius = Vector3.Distance(center, dot.transform.position);
-                    //angle = Vector2.Angle(center, dir);
-                    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-                    prevAng = angle;
-                    if (angle > -70 && angle < 64)
-                    {
-                        if (angle - prevAng >= 0)
-                        {
-                            Debug.Log("hey");
-                            anim.speed = 0;
-                            count += 0.003f;
-                            if(count < 0) count = 0;
-                            anim.Play("PROMLeftArm", 0, count);
-                        }
-                        else
-                        {
-                            Debug.Log("ho");
-                            anim.speed = 0;
-                            count -= 0.003f;
-                            if (count < 0) count = 0;
-                            anim.Play("PROMLeftArm", 0, count);
-                        }
-                        dot.transform.position = (Vector2)center + ((Vector2)dir * radius);
-                        Debug.Log(dir);
-                        Debug.Log(dot.transform.position);
-                        Debug.Log(angle);
-                    }
-                }
+                player = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+                //Debug.Log(camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)));
+                //iTween.PutOnPath(gameObject, iTweenPath.GetPath("Path1"), 50);
+                //MovetoMouse(player);
+                //iTween.MoveUpdate(gameObject,pos,0.001f);
+                iTween.PutOnPath(gameObject, iTweenPath.GetPath("Path1"), MovetoMouse(player)+0.2f);
+                //Vector3 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                //Debug.Log(p);
+                anim.speed = 0;
+                Debug.Log(MovetoMouse(player));
+                anim.Play("PROMRightArm", 0, MovetoMouse(player));
             }
-
         }
     }
-    void MathStuff()
+    float MovetoMouse(Vector3 Player)
     {
+        float minDistance = float.PositiveInfinity;
+        float minPercent = 0;
 
+        for (float t = 0; t <= 1; t += 0.005f)
+        {
+            float dist = ((Vector2)Player - (Vector2)iTween.PointOnPath(iTweenPath.GetPath("Path1"), t)).sqrMagnitude;
+            if (dist < minDistance)
+            {
+                minDistance = dist;
+                minPercent = t;
+                pos = iTween.PointOnPath(iTweenPath.GetPath("Path1"), t);
+            }
+            
+
+        }
+        return minPercent;
+        //iTween.PutOnPath(gameObject, iTweenPath.GetPath("Path1"), 50f);
+        Debug.Log(minPercent); 
     }
-
 }
-*/
 
 
