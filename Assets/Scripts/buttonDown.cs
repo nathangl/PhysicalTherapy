47fe4bd54c25576b92a8aece5f85b1e9e89815f3
Patrick Sorrell <Patrick Sorrell>
2017-03-02 21:49:24 -0500

    Camera Rotation

---
+++ Assets/Scripts/buttonDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class buttonDown : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+    public bool mouseDown = false;
+	public void OnPointerDown(PointerEventData eventData)
+    {
+        mouseDown = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        mouseDown = false;
+        Debug.Log(mouseDown);
+    }
+}
