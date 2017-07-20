01a95e69047c4624f370afb98bd9470e4a4ec6ff
nathangl <nathangl@sbcglobal.net>
2017-07-19 22:14:05 -0400

    Findings Update
    New section of Findings

--- Assets/Scripts/FindingsManager.cs
+++ Assets/Scripts/FindingsManager.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class FindingsManager : MonoBehaviour {
 
+    public string mode = "Deficit";
+
+    public GameObject Options;
+    public Toggle toggle;
+
+
+    //FOR TRANSFERS
     public List<string> amount = new List<string>{ "Independent", "Supervision", "Stand-by Assist", "Contact Guard Assist", "Minimal Assistance", "Moderate Assistance",
         "Maximal Assistance", "Dependent", "Visual cues", "Tactile cues" };
 
@@ -19,7 +26,7 @@ public class FindingsManager : MonoBehaviour {
 
     public List<string> position = new List<string>{ "left side", "right side", "front", "behind" };
 
-    public List<string> equipment = new List<string>{ "Gait Belt", "Sliding Board", "Rolling Walker", "Standard Walker", "Quad Cane", "Parallel bars", "Mirror-correct",
+    public List<string> equipment = new List<string>{ "Gait Belt", "Sliding Board", "Rolling Walker", "Standard Walker", "Quad Cane", "Parallel bars", "Mirror",
         "Hemi-sling" };
 
     Dictionary<string, List<int>> selected = new Dictionary<string, List<int>>()
@@ -31,6 +38,23 @@ public class FindingsManager : MonoBehaviour {
         {"Walking", new List<int>{0, 0, 0, 0}}
     };
 
+    //FOR DEFICITS
+    public List<string> ROMS = new List<string>{ "Right UE: Within Functional Limits", "Left UE: Within Functional Limits", "Right LE: Within Functional Limits", "Left LE:  Within Functional Limits", "Right shoulder", "Right elbow",
+        "Right wrist/hand", "Left shoulder", "Left elbow", "Left wrist/hand" , "Right hip", "Right knee", "Right ankle", "Left hip", "Left knee", "Left ankle", "Cervical spine", "Lumbar-thoracic spine" };
+
+    public List<string> STRENGTH = new List<string>{ "Right UE: Grossly within functional limits", "Left UE:  Grossly within functional limits", "Right LE:  Grossly within functional limits", "Left UE:  Grossly within functional limits", "Right shoulder flexors/extensors", "Right shoulder abductors",
+        "Right shoulder rotators", "Right elbow flexors", "Right elbow extensors", "Right wrist flexors/extensors" , "Right hand grip strength", "Left shoulder flexors/abductors", "Left shoulder rotators", "Left elbow flexors", "Left elbow extensors", "Left wrist flexors/extensors", "Left hand grip strength", "Right hip flexors/extensors",
+        "Right hip abductors", "Right knee extensors/flexors", "Right ankle dorsiflexion/plantar flexion", "Left hip flexors/extensors", "Left hip abductors", "Left knee extensors/flexors", "Left ankle dorsiflexion/plantar flexion"};
+
+    public List<Toggle> children = new List<Toggle>();
+
+    Dictionary<string, List<bool>> toggled = new Dictionary<string, List<bool>>()
+    {
+        {"AROM", new List<bool>()},
+        {"PROM", new List<bool>()},
+        {"STRENGTH", new List<bool>()}
+    };
+
     public Button bed, sitstand, sitting, standing, walking, submit;
     public Text info;
     public Dropdown amountD, typeD, positionD, equipmentD;
@@ -39,24 +63,44 @@ public class FindingsManager : MonoBehaviour {
     void Start()
     {
         Default();
+        SetupSaves();
     }
 
     void Default()      //starts on transfer dropdowns
     {
-        button = "Transfer w/c to/from bed";
-        info.text = button;
-        amountD.AddOptions(amount);
-        equipmentD.AddOptions(equipment);
-        positionD.AddOptions(position);
-        typeD.AddOptions(type[button]);
+        if (mode == "Transfer")
+        {
+            button = "Transfer w/c to/from bed";
+            info.text = button;
+            amountD.AddOptions(amount);
+            equipmentD.AddOptions(equipment);
+            positionD.AddOptions(position);
+            typeD.AddOptions(type[button]);
+        }
+        else if (mode == "Deficit")
+        {
+            button = "AROM";
+            info.text = button;
+            LoadToggles();
+        }
+
     }
 
-    public void Button(string b)    //called by buttons
+    public void Button(string b)    //called by buttons in transfer menus
     {
         Save();
         button = b;
         UpdateUI();
         Load();
+    }
+
+    public void ButtonToggles(string b)     //called by buttons in deficit menus
+    {
+        SaveToggles();
+        button = b;
+        DeleteToggles();
+        LoadToggles();
+        info.text = button;
     }
 
     void UpdateUI()     //updates dropdown options
@@ -80,5 +124,91 @@ public class FindingsManager : MonoBehaviour {
         typeD.value = selected[button][1];
         positionD.value = selected[button][2];
         equipmentD.value = selected[button][3];
+    }
+
+    void LoadToggles()      //loads toggles
+    {
+        if(button == "AROM" || button == "PROM")
+        {
+            for(int i = 0; i < ROMS.Count; i++)
+            {
+                Toggle temp = Instantiate(toggle);
+                temp.gameObject.GetComponentInChildren<Text>().text = ROMS[i];
+                temp.transform.SetParent(Options.transform);
+                try
+                {
+                    if (toggled[button][i])
+                        temp.isOn = true;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        else if(button == "STRENGTH")
+        {
+            for (int i = 0; i < STRENGTH.Count; i++)
+            {
+                Toggle temp = Instantiate(toggle);
+                temp.gameObject.GetComponentInChildren<Text>().text = STRENGTH[i];
+                temp.transform.SetParent(Options.transform);
+                try
+                {
+                    if (toggled[button][i])
+                        temp.isOn = true;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+
+    void SaveToggles()
+    {
+        foreach (Transform child in Options.transform)
+        {
+            children.Add(child.gameObject.GetComponent<Toggle>());
+        }
+        if(button == "AROM" || button == "PROM")
+        {
+            for (int i = 0; i < ROMS.Count; i++)
+            {
+                if (children[i].isOn)
+                    toggled[button][i] = true;
+            }
+        }
+        else if(button == "STRENGTH")
+        {
+            for (int i = 0; i < STRENGTH.Count; i++)
+            {
+                if (children[i].isOn)
+                    toggled[button][i] = true;
+            }
+        }
+    }
+
+    void SetupSaves()       //Setup lists for toggle saves
+    {
+        for (int i = 0; i < ROMS.Count; i++)
+        {
+            toggled["AROM"].Add(false);
+            toggled["PROM"].Add(false);
+        }
+        for (int i = 0; i < STRENGTH.Count; i++)
+        {
+            toggled["STRENGTH"].Add(false);
+        }
+    }
+
+    void DeleteToggles()
+    {
+        foreach (Transform child in Options.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        children.Clear();
     }
 }
