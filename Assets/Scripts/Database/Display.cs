a0194e6aae0de8474a93419520b782977ce6ca49
nathangl <nathangl@sbcglobal.net>
2017-08-03 20:23:27 -0400

    Animation Update
    Implemented all animations aside from a few PROM.

---
+++ Assets/Scripts/Database/Display.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour {
+
+    public Text text;
+    public int userID, logID;
+
+    void Start()
+    {
+        text.text = DatabaseManager.getLogSample(userID, logID);
+    }
+}
