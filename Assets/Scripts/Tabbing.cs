2489f59116c52a5c5785cbcb53813caad78a4d5f
[Evan Parduhn] <[parduhne@uindy.edu]>
2017-09-29 15:16:04 -0400

    Fixed Tabbing

--- Assets/Scripts/Tabbing.cs
+++ Assets/Scripts/Tabbing.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Tabbing : MonoBehaviour {
+public class Tabbing : MonoBehaviour
+{
+    EventSystem system;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        system = EventSystem.current;
+
+    }
+    // Update is called once per frame
+    public void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (next != null)
+            {
+
+                InputField inputfield = next.GetComponent<InputField>();
+                if (inputfield != null) inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+
+                system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+            }
+            //else Debug.Log("next nagivation element not found");
+
+        }
+    }
+
 }
