835780c7bff766ba8f8e8f7884f67f7e7b3d27b9
parduhne <31196250+parduhne@users.noreply.github.com>
2017-11-11 14:15:11 -0500

    Implemented iTween with VR
    iTween and VR are now working together, just a few issues.

---
+++ Assets/Scripts/VR/VRanimPath.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class VRanimPath : MonoBehaviour {
+
+	//FOR VR
+	private bool currentlyInteracting;
+	private WandInteractionVR attachedWand;
+	private Transform interactionPoint;
+	private float velocityFactor = 20000f;
+	private Vector3 posDelta;
+
+	//FOR ITWEEN
+	Vector3 player;
+	float minDistance = float.PositiveInfinity;
+	float minPercent = 0;
+	Vector3 pos;
+	public Animator anim;
+	bool active = false;
+	public animPathsManager manager;
+	float position;		//the position in animation or time in animation
+
+	// Use this for initialization
+	void Start () {
+		iTween.PutOnPath(gameObject, iTweenPath.GetPath(gameObject.name), 0);
+		interactionPoint = new GameObject().transform;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (attachedWand && currentlyInteracting)
+		{
+			if (gameObject.name == manager.prevAnim || manager.first == true)
+			{
+				//Use interactionPoint instead?
+//				Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
+//				Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+
+				float pos = DeterminePos(interactionPoint.position);
+				gameObject.transform.position = interactionPoint.position;
+
+				iTween.PutOnPath(gameObject, iTweenPath.GetPath(gameObject.name), pos);
+
+				position = (DeterminePos(interactionPoint.position)) / 2;
+				anim.Play(gameObject.name, 0, position);
+
+
+				manager.prevPos = position;
+				manager.prevAnim = gameObject.name;
+				manager.prevObj = gameObject;
+				manager.first = false;
+			}
+			else
+			{
+				iTween.PutOnPath(manager.prevObj, iTweenPath.GetPath(manager.prevAnim), 0);
+				manager.prevAnim = gameObject.name;
+			}
+		}
+	}
+
+	public void BeginInteraction(WandInteractionVR wand)
+	{
+		Debug.Log ("Begin Interaction Called!");
+		attachedWand = wand;
+		interactionPoint.position = wand.transform.position;
+		interactionPoint.rotation = wand.transform.rotation;
+
+		//interactionPoint.SetParent(transform, true);
+		//this.transform.SetParent(wand.transform);
+		currentlyInteracting = true;
+
+	}
+
+	public void EndInteraction(WandInteractionVR wand)
+	{
+		Debug.Log ("End Interaction Called!");
+		if (wand == attachedWand)
+		{
+			attachedWand = null;
+			currentlyInteracting = false;
+			//this.transform.SetParent(null);
+		}
+	}
+
+	public bool IsInteracting()
+	{
+		return currentlyInteracting;
+	}
+
+	float DeterminePos(Vector3 input)
+	{
+		float minDistance = float.PositiveInfinity;
+		float minPercent = 0;
+
+		for (float t = 0; t <= 1; t += 0.005f)
+		{
+			//float dist = ((Vector2)input - (Vector2)iTween.PointOnPath(iTweenPath.GetPath("PROMRightArm"), t)).sqrMagnitude;
+			float dist = ((Vector2)input - (Vector2)iTween.PointOnPath(iTweenPath.GetPath(gameObject.name), t)).sqrMagnitude;
+
+			if (dist < minDistance)
+			{
+				minDistance = dist;
+				minPercent = t;
+				pos = iTween.PointOnPath(iTweenPath.GetPath(gameObject.name), t);
+			}
+		}
+		//Debug.Log(minPercent);
+		return minPercent;
+	}
+}
---
+++ Assets/Scripts/VR/WandInteractionVR.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandInteractionVR : MonoBehaviour {
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    private VRanimPath animation;
+
+	private VRanimPath interactingItem;
+
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        if ( controller.GetPressDown(triggerButton))
+        {
+			if (interactingItem)
+			{
+				if (interactingItem.IsInteracting())
+				{
+					interactingItem.EndInteraction(this);
+				}
+				interactingItem.BeginInteraction(this);
+
+			}
+        }
+		if (controller.GetPressUp(triggerButton) && animation != null)
+        {
+            
+			//interactingItem.EndInteraction(this);            
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+		VRanimPath collidedItem = collider.GetComponent<VRanimPath>();
+		if (collidedItem)
+		{
+			interactingItem = collidedItem;
+		}
+        //InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+		Debug.Log (collider);
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+		interactingItem = null;
+        Debug.Log ("Wand exit");
+    }
+}
